8fd4ca9ed868f40da26216b019286bf18a09a287
ccxadmin <1530483594@qq.com>
2020-10-14 22:54:04 +0800

    hvlayout

--- mainwinTest.py
+++ mainwinTest.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'ccxadmin'
+
 import sys
-import MainwinLayout
+import MainwinHVCombine
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainwindow = QMainWindow()
-    ui = MainwinLayout.Ui_MainWindow()
+    ui = MainwinHVCombine.Ui_MainWindow()
     ui.setupUi(mainwindow)
     mainwindow.show()
     sys.exit(app.exec_())
