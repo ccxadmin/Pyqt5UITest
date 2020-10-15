292a61d8d4984d6e85eebc935196eb3da336ebcc
ccxadmin <1530483594@qq.com>
2020-10-15 10:42:14 +0800

    containerlayout

--- mainwinTest.py
+++ mainwinTest.py
@@ -6,13 +6,13 @@
 __author__ = 'ccxadmin'
 
 import sys
-import mainwinGridUI
+import ContainerUI
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mainwindow = QMainWindow()
-    ui = mainwinGridUI.Ui_MainWindow()
+    ui = ContainerUI.Ui_MainWindow()
     ui.setupUi(mainwindow)
     mainwindow.show()
     sys.exit(app.exec_())
