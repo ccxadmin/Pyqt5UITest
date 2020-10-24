f294a14428a9d7678da5317cc462556f42a3765e
ccxadmin <1530483594@qq.com>
2020-10-25 00:46:27 +0800

    ToolBarDemo

---
+++ ToolBarDemo.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys,math
+
+
+class ToolBarDemo(QMainWindow):
+    def __init__(self):
+        super(ToolBarDemo, self).__init__()
+        self.InitUI()
+        self.setWindowTitle('创建工具栏DEMO')
+        self.resize(500,500)
+
+
+    def InitUI(self):
+        # bar=QToolBar('file',self)
+        bar= self.addToolBar('file')
+        new =QAction(QIcon('new.png'),'new',self)
+        bar.addAction(new)
+        new2 = QAction(QIcon('save.png'), 'add', self)
+        bar.addAction(new2)
+        # bar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        bar2=self.addToolBar('硬件')
+        bar2.addAction(QIcon('resizeApi.png'),'CCD')
+
+        bar3 = QToolBar('file', self)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = ToolBarDemo()
+    main.show()
+    sys.exit(app.exec_())
